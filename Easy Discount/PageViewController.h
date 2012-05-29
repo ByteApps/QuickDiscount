968ebf5620e315ae9bb66a7ef7eb01810cf7afcc
Salvador Guerrero <salvador.icc@gmail.com>
2012-05-28 22:12:06 -0700

    Free app submitted for the first time

---
+++ Easy Discount/PageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageViewController.h
+//  Easy Discount
+//
+//  Created by Salvador Guerrero on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageViewController : UIPageViewController
+
+@end
---
+++ Easy Discount/PageViewController.m
@@ -0,0 +1,55 @@
+//
+//  PageViewController.m
+//  Easy Discount
+//
+//  Created by Salvador Guerrero on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PageViewController.h"
+#import "EasyDiscountViewController.h"
+
+@interface PageViewController ()
+
+@end
+
+@implementation PageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSMutableArray *controllers = [NSMutableArray arrayWithCapacity:2];
+    
+    EasyDiscountViewController *percentView = [self.storyboard instantiateViewControllerWithIdentifier:@"discount view"];
+    
+    [controllers addObject:percentView];
+    [controllers addObject:percentView.copy];
+    
+    [self setViewControllers:controllers direction:UIPageViewControllerNavigationOrientationHorizontal animated:YES completion:^(BOOL finished) {
+        //
+    }];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
